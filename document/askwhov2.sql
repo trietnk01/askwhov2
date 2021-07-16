8ebd580f20b5cfdfc07ee0c70b3092b3ef628068
Dien Nguyen <dien.nguyen@aegona.com>
2021-07-16 12:40:51 +0700

    Update code 16-07-2021-13h00

--- document/askwhov2.sql
+++ document/askwhov2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 16, 2021 lúc 07:38 AM
+-- Thời gian đã tạo: Th7 16, 2021 lúc 07:40 AM
 -- Phiên bản máy phục vụ: 10.4.19-MariaDB
 -- Phiên bản PHP: 7.4.20
 
