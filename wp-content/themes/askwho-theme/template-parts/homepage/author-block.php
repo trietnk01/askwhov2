bc59c1cd4f01749953b21a1a6651a66e4e952893
Dien Nguyen <dien.nguyen@aegona.com>
2021-07-17 12:32:32 +0700

    Update code 17-07-2021-13h00

--- wp-content/themes/askwho-theme/template-parts/homepage/author-block.php
+++ wp-content/themes/askwho-theme/template-parts/homepage/author-block.php
@@ -1,14 +1,22 @@
+<?php
+$hp_auteur_liste = get_field('hp_auteur_liste');
+if (is_array($hp_auteur_liste)) {
+    if (count($hp_auteur_liste) > 0) {
+        ?>
 <div class="owl_carousel_author owl-carousel owl-theme owl-loaded">
     <?php
-for ($i = 1; $i <= 16; $i++) {
-    ?>
+foreach ($hp_auteur_liste as $key => $val) {
+            ?>
     <div class="item">
-        <img class="author_image" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/author.jpg'; ?>"
-            alt="author" />
-        <div class="author_name">Prénom NOM</div>
-        <div class="author_function">Fonction</div>
+        <img class="author_image" src="<?php echo $val['hp_auteur_avatar']; ?>" alt="author" />
+        <div class="author_name"><?php echo $val['hp_auteur_nom']; ?></div>
+        <div class="author_function"><?php echo $val['hp_auteur_titre']; ?></div>
     </div>
     <?php
 }
-?>
+        ?>
 </div>
+<?php
+}
+}
+?>
--- wp-content/themes/askwho-theme/template-parts/homepage/featured-post-block.php
+++ wp-content/themes/askwho-theme/template-parts/homepage/featured-post-block.php
@@ -1,20 +1,38 @@
+<?php
+global $zController;
+$hp_article_liste = get_field('hp_article_liste');
+if (is_array($hp_article_liste)) {
+    if (count($hp_article_liste) > 0) {
+        ?>
 <div class="featured_post">
     <?php
-for ($i = 1; $i <= 4; $i++) {
-    ?>
+foreach ($hp_article_liste as $key => $val) {
+            $hp_article_objet = $val["hp_article_objet"];
+            $featured_image = '';
+            if (!empty(get_the_post_thumbnail_url($hp_article_objet->ID, 'full'))) {
+                $featured_image = get_the_post_thumbnail_url($hp_article_objet->ID, 'full');
+            } else {
+                $featured_image = get_stylesheet_directory_uri() . "/assets/images/no-image.png";
+            }
+            $date = get_the_date('Y-m-d', $hp_article_objet->ID);
+            $date_vn = $zController->getHelper("CommonHelper")->datetimeConverterByFormat($date, "d.m.Y");
+            ?>
     <div class="item">
-        <a href="javascript:void(0);" class="author_image">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/post.jpg'; ?>"
-                alt="<?php echo get_bloginfo("name"); ?>" />
+        <a href="<?php echo get_the_permalink($hp_article_objet->ID); ?>" class="author_image">
+            <img src="<?php echo $featured_image; ?>" alt="<?php echo get_bloginfo("name"); ?>" />
         </a>
-        <div class="post_date">26.04.2021</div>
+        <div class="post_date"><?php echo $date_vn; ?></div>
         <div class="post_title">
-            <a href="javascript:void(0);">
-                Ut lacinia nisl dolor interdum et malesuada
+            <a href="<?php echo get_the_permalink($hp_article_objet->ID); ?>">
+                <?php echo wp_trim_words($hp_article_objet->post_title, '99', '...'); ?>
             </a>
         </div>
     </div>
     <?php
 }
-?>
+        ?>
 </div>
+<?php
+}
+}
+?>
